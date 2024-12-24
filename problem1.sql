616cd9665c0be626f367bccf1c6519ffdfd4f30b
Brian Yang <yangbri1@msu.edu>
2024-12-24 00:52:16 +0000

    Implemented DDL DROP cmd to remove whole 'song' DB table & deallocate any memory associated with it (data, constraints,  structure, etc.)

---
+++ problem1.sql
@@ -0,0 +1,11 @@
+DROP TABLE song;
+-- DDL DROP cmd deallocates all memory attached to the DB table (data, structure, etc.)
+/**
+     *      Task: Drop the table "song"
+     *      song Table Diagram:
+     *      |      title        |        artist         |
+     *      ---------------------------------------------
+     *      |'Let it be'        |'Beatles'              |
+     *      |'Hotel California' |'Eagles'               |
+     *      |'Kashmir'          |'Led Zeppelin'         |
+     */
